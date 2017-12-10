5bf203d1be3b72c037573a18860fe910322feb39
JackeyEting <1193767343@qq.com>
2017-12-10 05:03:23 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+	public function __construct(){
+		$this->middleware('guest',[
+			'only'=>['create']
+		]);
+	}
+
     public function create(){
     	return view('sessions.create');
     }
@@ -20,7 +26,7 @@ class SessionsController extends Controller
     	if (Auth::attempt($credentials,$request->has('remember'))) {
     		//登录成功后的操作
     		session()->flash('success','Welcome to here.');
-    		return redirect()->route('users.show',[Auth::user()]);
+    		return redirect()->intended(route('users.show',[Auth::user()]));
     	}else{
     		//登录失败后的操作
     		session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,16 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+	public function __construct(){
+		$this->middleware('auth', [
+			'except'=>['show','create','store','index']
+		]);
+
+		$this->middleware('guest',[
+			'only'=>['create']
+		]);
+	}
+
     public function create(){//user sign up
     	return view('users.create');
     }
@@ -31,4 +41,42 @@ class UsersController extends Controller
     	return redirect()->route('users.show',[$user]);//等同于route('users.shwo',[$user->id])
     }
 
+    //编辑用户的操作
+    public function edit(User $user){
+    	$this->authorize('update',$user);
+    	return view('users.edit',compact('user'));
+    }
+
+    //展示所有的用户
+    public function index(){
+    	$users = User::paginate(10);//paginate()指定每页生成的数量
+    	return view('users.index',compact('users'));
+    }
+
+    public function update(User $user,Request $request){
+    	$this->validate($request, [
+    		'name'=>'required|max:50',
+    		'password'=>'nullable|confirmed|min:6'
+    	]);
+
+    	$this->authorize('update',$user);
+
+    	$data=[];
+    	$data['name']=$request->name;
+    	if ($request->password) {
+    		$data['password']=$request->password;
+    	}
+
+    	$user->update($data);
+    	session()->flash('success','change password success.');
+    	return redirect()->route('users.show',$user->id);
+    }
+
+    //进行用户的删除
+    public function destroy(User $user){
+    	$this->authorize('destroy',$user);
+    	$user->delete();
+    	session()->flash('success','成功删除用户');
+    	return back();
+    }
 }
